a8a167ff68e32302f940d0f68ed7d9e8926edba0
Varsha-Verma <varshaverma0108@gmail.com>
2023-01-13 00:18:37 +0530

    Script to create directories

---
+++ 2023/day05/createDirectories.sh
@@ -0,0 +1,11 @@
+#This script is written by Varsha Verma
+
+#!/bin/bash
+echo "Directories to be created by name $1, starting from $2 till $3"
+#cd /home/ubuntu/Devops/day5/
+cd /c/Users/varsha.a.verma/OneDrive - Accenture/varsha/Learning/git/90DaysofDevOps/2023/day5/
+for ((i=$2;i<=$3;i++))
+do
+        mkdir "$1$i"
+done
+echo "Your directories are ready!!!!"
